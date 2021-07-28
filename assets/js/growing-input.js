1e55bd5d84f54811e915357dbde491b41c9aeeca
Anno Ryota <62051551+AnnoRyota@users.noreply.github.com>
2021-07-28 17:11:41 +0900

    Add files via upload

--- assets/js/growing-input.js
+++ assets/js/growing-input.js
@@ -7,8 +7,9 @@ ta.addEventListener('keydown', autosize);
 function autosize() {
   setTimeout(function () {
     ta.style.cssText = 'height:0px'; // 文字を削除した場合にscrollHeightを縮めるためにこれが必要
-    var height = Math.min(22 * 3, ta.scrollHeight);
-    div.style.cssText = 'height:' + height + 'px';
+    ta.style.cssText = 'border-radius:50px';
+    var height = Math.min(22 * 4, ta.scrollHeight);
+    div.style.cssText = 'height:' + height * 1.75 + 'px';
     inner.style.cssText = 'height:' + height + 'px';
     ta.style.cssText = 'height:' + height + 'px';
   }, 0);
