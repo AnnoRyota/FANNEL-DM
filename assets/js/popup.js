23d6de2f92cb49ad718253c7f83d6e10ba714682
Anno Ryota <62051551+AnnoRyota@users.noreply.github.com>
2021-08-02 17:14:57 +0900

    Add files via upload

---
+++ assets/js/popup.js
@@ -0,0 +1,9 @@
+function popUp() {
+  var popup = document.getElementById("sub1");
+  popup.classList.toggle("show");
+}
+
+function popUp2() {
+  var popup = document.getElementById("sub2");
+  popup.classList.toggle("show");
+}
